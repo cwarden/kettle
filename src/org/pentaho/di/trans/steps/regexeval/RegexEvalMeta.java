cwarden/kettle

commit f76372ebc1ec559a134b1d90c4a339c3b1f73746

PDI-1101 : Wrong handling of UTF-8 characters in slave server remote execution
git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@7676 5fb7f6ec-07c1-534a-b4ca-9155e429e800

src/org/pentaho/di/www/AddTransServlet.java
@@ -12,8 +12,8 @@
 */
 package org.pentaho.di.www;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
@@ -62,7 +62,8 @@ public class AddTransServlet extends HttpServlet
         boolean useXML = "Y".equalsIgnoreCase( request.getParameter("xml") );
         
         PrintWriter out = response.getWriter();
-        InputStream is = request.getInputStream(); // read from the client
+        BufferedReader in = request.getReader();
+        if (log.isDetailed()) log.logDetailed(toString(), "Encoding: "+request.getCharacterEncoding());
 
         if (useXML)
         {
@@ -81,12 +82,12 @@ public class AddTransServlet extends HttpServlet
 
         try
         {
-            // First read the complete transformation in memory from the inputStream
-            int c;
+            // First read the complete transformation in memory from the request
             StringBuffer xml = new StringBuffer();
-            while ( (c=is.read())!=-1)
+            String line;
+            while ((line = in.readLine()) != null)
             {
-                xml.append((char)c);
+                xml.append(line);
             }
             
             // Parse the XML, create a transformation configuration

src/org/pentaho/di/cluster/SlaveServer.java
@@ -25,6 +25,7 @@ import java.net.URLConnection;
 import java.util.List;
 import java.util.Map;
 
+import org.apache.commons.httpclient.Header;
 import org.apache.commons.httpclient.HttpClient;
 import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.UsernamePasswordCredentials;
@@ -385,7 +386,8 @@ public class SlaveServer extends ChangedFlag implements Cloneable, SharedObjectI
         
         put.setRequestEntity(entity);
         put.setDoAuthentication(true);
-        
+        put.addRequestHeader(new Header("Content-Type", "text/xml;charset=" + Const.XML_ENCODING));
+
         // post.setContentChunked(true);
         
         // Get HTTP client

src/org/pentaho/di/www/AddJobServlet.java
@@ -12,8 +12,8 @@
 */
 package org.pentaho.di.www;
 
+import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.PrintWriter;
 import java.util.Arrays;
 import java.util.Map;
@@ -65,8 +65,9 @@ public class AddJobServlet extends HttpServlet
         boolean useXML = "Y".equalsIgnoreCase( request.getParameter("xml") );
         
         PrintWriter out = response.getWriter();
-        InputStream is = request.getInputStream(); // read from the client
-
+        BufferedReader in = request.getReader(); // read from the client
+        if (log.isDetailed()) log.logDetailed(toString(), "Encoding: "+request.getCharacterEncoding());
+        
         if (useXML)
         {
             response.setContentType("text/xml");
@@ -84,12 +85,12 @@ public class AddJobServlet extends HttpServlet
 
         try
         {
-            // First read the complete job in memory from the inputStream
-            int c;
+            // First read the complete transformation in memory from the request
             StringBuffer xml = new StringBuffer();
-            while ( (c=is.read())!=-1)
+            String line;
+            while ((line = in.readLine()) != null)
             {
-                xml.append((char)c);
+                xml.append(line);
             }
             
             // Parse the XML, create a job configuration

commit 8f7d1667f42724b3f367020adf02193551a86b2c

PDI-1243 -- Chage addTagValue() to plain open/closeTag() in RegexEval script because addTagValue escapes characters even inside CDATA
Add linefeeds back into XML parsed from Trans upload.  This was actually a regression introduced in r7676 for bug PDI-1101

git-svn-id: svn://source.pentaho.org/svnkettleroot/Kettle/trunk@7828 5fb7f6ec-07c1-534a-b4ca-9155e429e800

src/org/pentaho/di/trans/steps/regexeval/RegexEvalMeta.java
@@ -451,7 +451,7 @@ public class RegexEvalMeta extends BaseStepMeta implements StepMetaInterface
     {
         StringBuilder retval = new StringBuilder();
 
-        retval.append("    " + XMLHandler.addTagValue("script", XMLHandler.buildCDATA(script))); //$NON-NLS-1$ //$NON-NLS-2$
+        retval.append("    " + XMLHandler.openTag("script") + XMLHandler.buildCDATA(script) + XMLHandler.closeTag("script")); //$NON-NLS-1$ //$NON-NLS-2$
         retval.append("    " + XMLHandler.addTagValue("matcher", matcher)); //$NON-NLS-1$ //$NON-NLS-2$
         retval.append("    " + XMLHandler.addTagValue("resultfieldname", resultfieldname)); //$NON-NLS-1$ //$NON-NLS-2$
         retval.append("    " + XMLHandler.addTagValue("usevar", usevar));

src/org/pentaho/di/www/AddTransServlet.java
@@ -83,11 +83,11 @@ public class AddTransServlet extends HttpServlet
         try
         {
             // First read the complete transformation in memory from the request
-            StringBuffer xml = new StringBuffer();
+            StringBuilder xml = new StringBuilder(request.getContentLength());
             String line;
             while ((line = in.readLine()) != null)
             {
-                xml.append(line);
+                xml.append(line).append(Const.CR);
             }
             
             // Parse the XML, create a transformation configuration